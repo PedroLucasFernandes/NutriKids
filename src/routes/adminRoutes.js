2ca5b495237c04faf785e5c812d46535572a33ed
aanitakawasaki <aanitakawasaki@gmail.com>
2024-04-23 10:31:17 -0300

    corrigindo rotas (tornando-as menos específicas: /, /:id) para corrigir o authMiddleware

--- src/routes/adminRoutes.js
+++ src/routes/adminRoutes.js
@@ -5,8 +5,8 @@ const adminController = require('../controllers/adminController.js');
 const router = express.Router();
 //criando um objeto router que contém as funcionalidades de roteamento do express.
 
-router.post('/admin', adminController.loginAdmin);
-router.get('/admin', adminController.confirmLogin);
+router.post('/', adminController.loginAdmin);
+router.get('/', adminController.confirmLogin);
 //definindo as rotas /admin e /admin com os métodos post e get, respectivamente. o método post é responsável por autenticar o usuário, enquanto o método get é responsável por confirmar se o usuário está autenticado.
 
 module.exports = router;
